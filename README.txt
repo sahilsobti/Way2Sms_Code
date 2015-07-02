be9c818f7e2bfadc0546222514bedfc750db3306
sahilsobti <sahilsobti92@gmail.com>
2015-07-02 15:19:03 +0530

    code added

---
+++ README.txt
@@ -0,0 +1,3 @@
+This is a program to send messages through way2sms.
+The program is written in python.
+Fill in your username, password , number where you want to send the message and the message in the program.
---
+++ sms.py
@@ -0,0 +1,47 @@
+import com.gym.fitnesszone
+import urllib2
+import cookielib
+from getpass import getpass
+import sys
+import os
+from stat import *
+
+if __name__ == "__main__":    
+	username = "ur way2sms no"
+	passwd = "your password"
+
+	#message = "Fitness Zone Reminder: \n\n" + sys.argv[1] +" Your last month payment got over by " + sys.argv[3] +". Pay for the next month today. Thank you "
+	message = "your message"
+	number = "Where to send the message"
+
+	message = "+".join(message.split(' '))
+
+#logging into the sms site
+	url ='http://site24.way2sms.com/Login1.action?'
+	data = 'username='+username+'&password='+passwd+'&Submit=Sign+in'
+ 
+ #For cookies
+ 
+	cj= cookielib.CookieJar()
+	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+ 
+ #Adding header details
+	opener.addheaders=[('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120')]
+	try:
+		usock =opener.open(url, data)
+	except IOError:
+		print "error"
+		return()
+	
+	jession_id =str(cj).split('~')[1].split(' ')[0]
+	send_sms_url = 'http://site24.way2sms.com/smstoss.action?'
+	send_sms_data = 'ssaction=ss&Token='+jession_id+'&mobile='+number+'&message='+message+'&msgLen=136'
+	opener.addheaders=[('Referer', 'http://site25.way2sms.com/sendSMS?Token='+jession_id)]
+	try:
+		sms_sent_page = opener.open(send_sms_url,send_sms_data)
+	except IOError:
+		print "error"
+		return()
+		
+	print "success"	
+	return ()
